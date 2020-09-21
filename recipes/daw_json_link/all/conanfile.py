ae5554470426ded2b7984cc2bc413be74525eee8
Chris Mc <prince.chrismc@gmail.com>
2020-09-21 19:55:41 -0400

    starting a daw_json_link recipe

---
+++ recipes/daw_json_link/all/conanfile.py
@@ -0,0 +1,82 @@
+from conans import ConanFile, tools, CMake
+from conans.errors import ConanInvalidConfiguration
+import os
+
+
+class DawJsonConan(ConanFile):
+    name = "daw_json_link"
+    homepage = "https://https://github.com/beached/daw_json_link"
+    description = "Static JSON parsing in C++"
+    topics = ("conan", "json", "parser", "serialize")
+    url = "https://github.com/conan-io/conan-center-index"
+    license = "BSL-1.0"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared":[True,False], "fPIC":[True,False]}
+    default_options = {"shared": False, "implicit_conversions": True}
+
+    _cmake = None
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
+
+    @property
+    def _minimum_cpp_standard(self):
+        return 17
+
+    @property
+    def _minimum_compilers_version(self):
+        # https://github.com/beached/daw_json_link#requirements
+        return {
+            "Visual Studio": "16",
+            "gcc": "8",
+            "clang": "7",
+            "apple-clang": "10",
+        }
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
+
+        if self.settings.get_safe("compiler.cppstd"):
+            tools.check_min_cppstd(self, self._minimum_cpp_standard)
+        min_version = self._minimum_compilers_version.get(str(self.settings.compiler))
+        if not min_version:
+            self.output.warn("{} recipe lacks information about the {} compiler support.".format(self.name, self.settings.compiler))
+        else:
+            if tools.Version(self.settings.compiler.version) < min_version:
+                raise ConanInvalidConfiguration("{} requires C++{} support. The current compiler {} {} does not support it.".format(
+                    self.name, self._minimum_cpp_standard, self.settings.compiler, self.settings.compiler.version))
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_dir = self.name + "-" + self.version
+        os.rename(extracted_dir, self._source_subfolder)
+
+    def _configure_cmake(self):
+        if not self._cmake:
+            self._cmake = CMake(self)
+            # self._cmake.definitions["JSON_BuildTests"] = False
+            self._cmake.configure(source_folder=self._source_subfolder)
+        return self._cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
+        cmake = self._configure_cmake()
+        cmake.install()
+        tools.rmdir(os.path.join(self.package_folder, "lib"))
+
+    def package_info(self):
+        pass
---
+++ recipes/daw_json_link/all/test_package/test_package.cpp
@@ -0,0 +1,136 @@
+// https://github.com/beached/daw_json_link/blob/691c933e8f7c703f4aea55f12ac3bf24383b037e/tests/src/cookbook_enums1_test.cpp
+
+#include "defines.h"
+
+#include "daw/json/daw_json_link.h"
+
+#include <daw/daw_read_file.h>
+
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+namespace daw::cookbook_enums1
+{
+    enum class Colours : uint8_t
+    {
+        red,
+        green,
+        blue,
+        black
+    };
+
+    DAW_CONSTEXPR std::string_view to_string(Colours c)
+    {
+        switch (c)
+        {
+        case Colours::red:
+            return "red";
+        case Colours::green:
+            return "green";
+        case Colours::blue:
+            return "blue";
+        case Colours::black:
+            return "black";
+        }
+        std::abort();
+    }
+
+    DAW_CONSTEXPR Colours from_string(daw::tag_t<Colours>,
+                                      std::string_view sv)
+    {
+        if (sv == "red")
+        {
+            return Colours::red;
+        }
+        if (sv == "green")
+        {
+            return Colours::green;
+        }
+        if (sv == "blue")
+        {
+            return Colours::blue;
+        }
+        if (sv == "black")
+        {
+            return Colours::black;
+        }
+        std::abort();
+    }
+
+    struct MyClass1
+    {
+        std::vector<Colours> member0;
+    };
+
+    bool operator==(MyClass1 const &lhs, MyClass1 const &rhs)
+    {
+        return lhs.member0 == rhs.member0;
+    }
+} // namespace daw::cookbook_enums1
+
+namespace daw::json
+{
+    template <>
+    struct json_data_contract<daw::cookbook_enums1::MyClass1>
+    {
+#ifdef __cpp_nontype_template_parameter_class
+        using type = json_member_list<json_array<
+            "member0", json_custom<no_name, daw::cookbook_enums1::Colours>>>;
+#else
+        static DAW_CONSTEXPR inline char const member0[] = "member0";
+        using type = json_member_list<
+            json_array<member0, json_custom<no_name, daw::cookbook_enums1::Colours>>>;
+#endif
+        static inline auto
+        to_json_data(daw::cookbook_enums1::MyClass1 const &value)
+        {
+            return std::forward_as_tuple(value.member0);
+        }
+    };
+} // namespace daw::json
+
+int main(int argc, char **argv)
+try
+{
+    if (argc <= 1)
+    {
+        puts("Must supply path to cookbook_enums1.json file\n");
+        exit(EXIT_FAILURE);
+    }
+    auto data = R"##({
+  "member0": [
+    "red",
+    "green",
+    "blue",
+    "black"
+  ]
+})##";
+
+    auto const cls = daw::json::from_json<daw::cookbook_enums1::MyClass1>(
+        std::string_view(data.data(), data.size()));
+
+    daw_json_assert(cls.member0[0] == daw::cookbook_enums1::Colours::red,
+                    "Unexpected value");
+    daw_json_assert(cls.member0[1] == daw::cookbook_enums1::Colours::green,
+                    "Unexpected value");
+    daw_json_assert(cls.member0[2] == daw::cookbook_enums1::Colours::blue,
+                    "Unexpected value");
+    daw_json_assert(cls.member0[3] == daw::cookbook_enums1::Colours::black,
+                    "Unexpected value");
+    auto const str = daw::json::to_json(cls);
+    puts(str.c_str());
+
+    auto const cls2 = daw::json::from_json<daw::cookbook_enums1::MyClass1>(
+        std::string_view(str.data(), str.size()));
+
+    daw_json_assert(cls == cls2, "Unexpected round trip error");
+}
+catch (daw::json::json_exception const &jex)
+{
+    std::cerr << "Exception thrown by parser: " << jex.reason() << std::endl;
+    exit(1);
+}
